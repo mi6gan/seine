6b78e6be10b9bd3760eca8f1b7fc21f2534eafdd
Mikhail Boyarov <mi6gan@mail.ru>
2020-02-26 13:18:15 +0300

    seine-43 Add bream related stories

--- packages/styles/src/bream/SuiteLogsBCGLayout.js
+++ packages/styles/src/bream/SuiteLogsBCGLayout.js
@@ -8,7 +8,6 @@ import BootstrapRow from './BootstrapRow';
 import ThemePaper from './ThemePaper';
 import BootstrapColumn from './BootstrapColumn';
 import BootstrapBox from './BootstrapBox';
-import SuiteLogsHeader from './SuiteLogsHeader';
 import BootstrapFlex from './BootstrapFlex';
 
 type Props = {
@@ -18,9 +17,11 @@ type Props = {
   children: React.Node,
 };
 
-const ContentPaper = ({ forwardedAs = 'section', ...props }) => (
-  <ThemePaper forwardedAs={forwardedAs} {...props} />
-);
+const ContentPaper = ({
+  as = BootstrapBox,
+  forwardedAs = 'section',
+  ...props
+}) => <ThemePaper as={as} forwardedAs={forwardedAs} {...props} />;
 
 const StyledDescriptionPaper = styled(ThemePaper)`
   ${({ theme: { breakpoints } }) => css`
@@ -37,11 +38,40 @@ const DescriptionPaper = ({ forwardedAs = 'section', ...props }) => (
   <StyledDescriptionPaper forwardedAs={forwardedAs} {...props} />
 );
 
-const SubtitleTypography = ({
+const TitleTypography = ({
   as = BootstrapBox,
-  forwarderAs = 'h4',
+  forwardedAs = 'h3',
   ...props
-}) => <BootstrapTypography as={as} forwardedAs={forwarderAs} {...props} />;
+}) => <BootstrapTypography as={as} forwardedAs={forwardedAs} {...props} />;
+
+const SubtitleTypography = styled(
+  ({ as = BootstrapBox, forwarderAs = 'h4', ...props }) => (
+    <BootstrapTypography as={as} forwardedAs={forwarderAs} {...props} />
+  )
+)`
+  ${({ theme: { breakpoints } }) => css`
+    ${breakpoints.up('lg')} {
+      text-align: center;
+    }
+    ${breakpoints.down('md')} {
+      text-align: left;
+    }
+  `}
+`;
+
+const SubtitleRuler = styled(BootstrapBox).attrs(
+  ({ width = '100%', height = 1 }) => ({
+    height,
+    width,
+  })
+)`
+  ${({ theme: { breakpoints, bootstrap } }) => css`
+    ${breakpoints.down('md')} {
+      display: none;
+    }
+    background-color: ${bootstrap.borderColor};
+  `}
+`;
 
 /**
  * @description Layout of BCG page.
@@ -57,18 +87,29 @@ export default function SuiteLogsBCGLayout({
 }: Props) {
   return (
     <BootstrapContainer as={as}>
-      <SuiteLogsHeader>{title}</SuiteLogsHeader>
+      <BootstrapRow as={'header'} paddingY={2} paddingX={5}>
+        <BootstrapColumn
+          as={ThemePaper}
+          xs={12}
+          marginBottom={3}
+          paddingY={7}
+          width={'100%'}
+          flexShrink={10}
+          forwardedAs={TitleTypography}
+          variant={'h3'}
+          fontWeight={600}
+        >
+          {title}
+        </BootstrapColumn>
+      </BootstrapRow>
 
       <BootstrapRow as={'article'} marginBottom={7} paddingX={5}>
-        <BootstrapColumn
-          xs={12}
-          lg={4}
-          as={ContentPaper}
-          forwardedAs={BootstrapBox}
-          paddingY={7}
-        >
-          {children}
+        <BootstrapColumn xs={12} lg={4} as={ContentPaper} paddingY={7}>
+          <BootstrapBox as={BootstrapTypography} variant={'body1'} paddingX={5}>
+            {children}
+          </BootstrapBox>
         </BootstrapColumn>
+
         <BootstrapColumn
           xs={12}
           lg={8}
@@ -77,15 +118,19 @@ export default function SuiteLogsBCGLayout({
           paddingY={7}
         >
           <BootstrapFlex flexDirection={'column'} width={'100%'}>
-            <ThemePaper
+            <BootstrapBox
               as={SubtitleTypography}
               fontWeight={600}
               variant={'h4'}
               width={'100%'}
+              paddingX={5}
+              textAlign={'center'}
             >
               {subtitle}
-            </ThemePaper>
-            <BootstrapBox as={BootstrapTypography} marginY={7}>
+            </BootstrapBox>
+            <SubtitleRuler marginTop={7} />
+
+            <BootstrapBox as={BootstrapTypography} marginY={7} paddingX={5}>
               {description}
             </BootstrapBox>
           </BootstrapFlex>
