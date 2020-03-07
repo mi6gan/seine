6fa1da3c63d6f26f39ffee826a503f04427929b6
Mikhail Boyarov <mi6gan@mail.ru>
2020-03-07 23:06:36 +0300

    cache rollup build

--- scripts/resolve-workspaces.js
+++ scripts/resolve-workspaces.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 const { join, resolve } = require('path');
 const { inspect } = require('util');
+const crypto = require('crypto');
+
+const { hashElement } = require('folder-hash');
+const findCacheDir = require('find-cache-dir');
 
 const parseWorkspaces = require('./parse-workspaces');
 
@@ -8,7 +12,7 @@ const parseWorkspaces = require('./parse-workspaces');
  * @description Resolve each workspaces to its context path, entry module and
  * packageJson content
  * @param {string[]} workspaces
- * @returns {Array<{context: string, entry: string, packageJson: object}>}
+ * @returns {Array<{context: string, entry: string, resolveCachePath: function(string): Promise<string>, packageJson: object}>}
  */
 function resolveWorkspaces(workspaces) {
   return parseWorkspaces(workspaces).packages.map((workspace) => {
@@ -24,6 +28,17 @@ function resolveWorkspaces(workspaces) {
         context,
         entry: require.resolve(join(context, 'src', 'index.js')),
         packageJson,
+        resolveCachePath: async (name) =>
+          findCacheDir({
+            name: join(
+              packageJson.name,
+              crypto
+                .createHash('md5')
+                .update((await hashElement(join(context, 'src'))).toString())
+                .digest('hex'),
+              name
+            ),
+          }),
       };
     } catch (err) {
       return { context, packageJson };
