5148b387d6c3b57fc32a2142647b60728a647d46
Mikhail Boyarov <mi6gan@mail.ru>
2020-03-22 23:20:16 +0300

    bream#850 Pie chart: вывод тайтлов согласно макету

--- packages/charts/src/useChartSvgProps.js
+++ packages/charts/src/useChartSvgProps.js
@@ -21,16 +21,21 @@ export default function useChartSvgProps(
     ...{ parentType: string },
   }
 ) {
-  if (chart.parentType === 'grid') {
-    return defaultSvgProps;
-  }
   switch (kind) {
     case chartTypes.BAR: {
+      if (chart.parentType === 'grid') {
+        return defaultSvgProps;
+      }
       const height =
         (chart.elements.length * VIEWPORT_HEIGHT) /
         Math.max(chart.elements.length, 8);
       return {
         viewBox: `0 ${VIEWPORT_HEIGHT - height} ${VIEWPORT_WIDTH} ${height}`,
+      };
+    }
+    case chartTypes.PIE: {
+      return {
+        overflow: 'visible',
       };
     }
     default:
