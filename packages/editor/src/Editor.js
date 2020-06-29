a9fef9019726f1a8bbf5032ea1c9d5ef2df806bd
Mikhail Boyarov <mi6gan@mail.ru>
2020-06-30 00:30:28 +0300

    next#1 reset editor

--- packages/editor/src/Editor.js
+++ packages/editor/src/Editor.js
@@ -1,204 +1,124 @@
 // @flow
 import * as React from 'react';
-import styled, { css } from 'styled-components/macro';
-import { ClickAwayListener, Paper } from '@material-ui/core';
-import type { ContentProps } from '@seine/content';
-import { Content, defaultBlockRenderMap, Grid, Page } from '@seine/content';
-import type {
-  AddButtonProps,
-  Block,
-  BlockEditor,
-  BlocksAction,
-  BlocksState,
-  BlockType,
-  ToolbarProps,
-} from '@seine/core';
-import {
-  blockTypes,
-  DESELECT_ALL_BLOCKS,
-  initialBlocksState,
-  reduceBlocks,
-} from '@seine/core';
-import { ChartEditor, ChartToolbar } from '@seine/charts-editor';
-import { DraftEditor, DraftToolbar } from '@seine/draft-editor';
-import { BlockDeleteButton, useReducerEx } from '@seine/ui';
+import { Box, Button, Menu, MenuItem } from '@material-ui/core';
+import { GridOnSharp as GridIcon, Menu as MenuIcon } from '@material-ui/icons';
 import { ThemeProvider } from '@seine/styles';
-import { TableEditor, TableToolbar } from '@seine/tables-editor';
-import { useAutoCallback, useAutoEffect, useAutoMemo } from 'hooks.macro';
+import { useAutoCallback } from 'hooks.macro';
+import styled from 'styled-components/macro';
 
-import PieChartAddButton from './PieChartAddButton';
-import BarChartAddButton from './BarChartAddButton';
-import ColumnChartAddButton from './ColumnChartAddButton';
-import LineChartAddButton from './LineChartAddButton';
-import DraftAddButton from './DraftAddButton';
-import { ImageEditor } from './ImageEditor';
-import ImageToolbar from './ImageToolbar';
-import PageToolbar from './PageToolbar';
-import ImageAddButton from './ImageAddButton';
 import defaultTheme from './defaultTheme';
-import TableAddButton from './TableAddButton';
 
-const defaultEditorChildren = [];
-
-export const defaultEditorBlockRendererMap = {
-  ...defaultBlockRenderMap,
-  [blockTypes.CHART]: ChartEditor,
-  [blockTypes.RICH_TEXT]: DraftEditor,
-  [blockTypes.GRID]: ({ dispatch, editor, selection, ...props }) => (
-    <Grid {...props} />
-  ),
-  [blockTypes.PAGE]: ({
-    id,
-    addButtonRenderMap,
-    dispatch,
-    editor,
-    selection,
-    ...props
-  }: BlockEditor & Block) => <Page {...props} />,
-  [blockTypes.IMAGE]: ImageEditor,
-  [blockTypes.TABLE]: TableEditor,
-};
-
-export const defaultAddButtonRenderMap = {
-  [blockTypes.CHART]: (props: AddButtonProps) => (
-    <>
-      <PieChartAddButton {...props} />
-      <BarChartAddButton {...props} />
-      <ColumnChartAddButton {...props} />
-      <LineChartAddButton {...props} />
-    </>
-  ),
-  [blockTypes.RICH_TEXT]: DraftAddButton,
-  [blockTypes.GRID]: () => null,
-  [blockTypes.IMAGE]: ImageAddButton,
-  [blockTypes.PAGE]: () => null,
-  [blockTypes.TABLE]: TableAddButton,
-};
-
-export const defaultToolbarRenderMap = {
-  [blockTypes.CHART]: ChartToolbar,
-  [blockTypes.RICH_TEXT]: DraftToolbar,
-  [blockTypes.GRID]: PageToolbar,
-  [blockTypes.IMAGE]: ImageToolbar,
-  [blockTypes.PAGE]: PageToolbar,
-  [blockTypes.TABLE]: TableToolbar,
-};
-
-const DefaultContainer = styled.div`
-  width: 100%;
-`;
-
-const ContentPaper = styled(Paper)`
+const ToolbarButton = styled(Button).attrs(() => ({
+  color: 'inherit',
+}))`
   && {
-    ${({ theme }) => css`
-      max-height: ${20 * theme.spacing(6)}px;
-      min-height: ${2 * theme.spacing(6)}px;
-      padding: ${theme.spacing(6)}px;
-      margin-bottom: ${theme.spacing(6)}px;
-      overflow: hidden auto;
-    `}
+    ${({ selected, theme }) =>
+      selected && { backgroundColor: theme.palette.grey[800] }};
   }
 `;
 
-export type Props = {
-  as?: React.ComponentType<*>,
-  children?: Block[],
-  onChange: (Block[]) => any,
-  parent: Block,
-  addButtonRenderMap?: {
-    [BlockType]: React.ComponentType<AddButtonProps>,
-  },
-  toolbarRenderMap?: {
-    [BlockType]: React.ComponentType<ToolbarProps>,
-  },
-} & ContentProps;
+const Toolbar = styled(Box).attrs({
+  bgcolor: 'grey.700',
+  color: 'grey.50',
+  width: '100%',
+})`
+  ${ToolbarButton} {
+    border-radius: 0;
+    min-width: 0;
+  }
+`;
+
+const StyledMenu = styled(Box).attrs({ component: Menu })`
+  .MuiMenu-paper {
+    color: ${({ theme }) => theme.palette.grey[50]};
+    background-color: ${({ theme }) => theme.palette.grey[700]};
+    min-width: 10em;
+  }
+`;
+
+const Content = styled(Box).attrs({
+  minHeight: 480,
+  width: '80%',
+})`
+  ${({ cursor }) => cursor && { cursor }};
+`;
+
+const Sidebar = styled(Box).attrs({
+  bgcolor: 'grey.100',
+  color: 'grey.700',
+  minWidth: 150,
+  minHeight: 480,
+  square: true,
+})``;
 
 /**
  * @description Default content editor.
- * @param {Props} props
  * @returns {React.Node}
  */
-export default function Editor({
-  addButtonRenderMap = defaultAddButtonRenderMap,
-  as: Container = DefaultContainer,
-  blockRenderMap = defaultEditorBlockRendererMap,
-  children = defaultEditorChildren,
-  onChange,
-  parent,
-  toolbarRenderMap = defaultToolbarRenderMap,
-  ...contentProps
-}: Props) {
-  const init = useAutoCallback(() => ({
-    ...initialBlocksState,
-    blocks: children,
-  }));
-  const [{ blocks, selection }, dispatch] = useReducerEx<
-    BlocksState,
-    BlocksAction
-  >(reduceBlocks, initialBlocksState, init);
+export default function Editor() {
+  const [tool, setTool] = React.useState(null);
+  const closeMenu = useAutoCallback(() => setTool(null));
 
-  useAutoEffect(() => {
-    onChange(
-      // no extra data should be passed, like `editor` key value
-      blocks.map(({ id, body, format, parent_id, type }) => ({
-        body,
-        format,
-        id,
-        parent_id,
-        type,
-      }))
-    );
-  });
-
-  const { type, ...block } = useAutoMemo(
-    selection.length === 1
-      ? blocks.find(({ id }) => selection.includes(id))
-      : parent
-  );
-
-  const BlockToolbar = toolbarRenderMap[type];
-
-  const contentChildren = useAutoMemo(
-    blocks.map((block) => ({
-      ...block,
-      addButtonRenderMap,
-      dispatch,
-      selection,
-    }))
-  );
-
-  const deselectClickHandler = useAutoCallback(
-    (event) =>
-      (event.target === event.currentTarget ||
-        event.target instanceof HTMLHtmlElement) &&
-      dispatch({ type: DESELECT_ALL_BLOCKS })
-  );
+  const menuAnchorRef = React.useRef(null);
+  const gridIconRef = React.useRef(null);
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ClickAwayListener onClickAway={deselectClickHandler}>
-        <Container>
-          <BlockToolbar
-            {...block}
-            onClick={deselectClickHandler}
-            blocks={blocks}
-            addButtonRenderMap={addButtonRenderMap}
-            dispatch={dispatch}
-            selection={selection}
+      <StyledMenu
+        open={tool === 'menu'}
+        onClose={closeMenu}
+        anchorEl={menuAnchorRef.current}
+        keepMounted
+        mt={6}
+      >
+        <MenuItem onClick={closeMenu}>Copy</MenuItem>
+        <MenuItem onClick={closeMenu}>Undo</MenuItem>
+        <MenuItem onClick={closeMenu}>Redo</MenuItem>
+      </StyledMenu>
+
+      <Box
+        display={'flex'}
+        flexWrap={'wrap'}
+        alignItems={'flex-start'}
+        justifyContent={'space-between'}
+        bgcolor={'grey.300'}
+      >
+        <Toolbar>
+          <ToolbarButton
+            aria-label={'menu'}
+            onClick={useAutoCallback(() => setTool('menu'))}
+            selected={tool === 'menu'}
+            ref={menuAnchorRef}
           >
-            <BlockDeleteButton dispatch={dispatch} selection={selection} />
-          </BlockToolbar>
-          <ContentPaper onClick={deselectClickHandler}>
-            <Content
-              {...contentProps}
-              parent={parent}
-              blockRenderMap={blockRenderMap}
-            >
-              {contentChildren}
-            </Content>
-          </ContentPaper>
-        </Container>
-      </ClickAwayListener>
+            <MenuIcon />
+          </ToolbarButton>
+
+          <ToolbarButton
+            aria-label={'grid'}
+            onClick={useAutoCallback(() => setTool('grid'))}
+            selected={tool === 'grid'}
+            title={'Grid tool'}
+          >
+            <GridIcon
+              ref={gridIconRef}
+              fill={'white'}
+              width={24}
+              height={24}
+              xmlns="http://www.w3.org/2000/svg"
+            />
+          </ToolbarButton>
+        </Toolbar>
+
+        <Content
+          cursor={
+            tool === 'grid' &&
+            `url(data:image/svg+xml;base64,${btoa(
+              gridIconRef.current.outerHTML
+            )}), auto`
+          }
+        />
+        <Sidebar>&nbsp;</Sidebar>
+      </Box>
     </ThemeProvider>
   );
 }
--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -1,5 +1,3 @@
 // @flow
-export * from './Editor';
 export { default as Editor } from './Editor';
-export { default as ImageAddButton } from './ImageAddButton';
 export { default as defaultTheme } from './defaultTheme';
