18e8380cf506a78efb4ef416a7c61155633b33c2
Mikhail Boyarov <mi6gan@mail.ru>
2020-07-08 03:05:35 +0300

    migrate to yarn 2

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,15 +2,11 @@
 yarn config set changesetBaseRefs --json '["'"$TRAVIS_BRANCH"'"]'
 yarn config set npmAuthToken "$NPM_TOKEN"
 
-if ! yarn version check
+if [ -z "$TRAVIS_TAG" ]
 then
   mkdir -p .yarn/versions
   cp yarn-versions/"$TRAVIS_BRANCH".yml .yarn/versions/
   yarn version apply --all
-fi
-
-if [ -z "$TRAVIS_TAG" ]
-then
   git tag "$(node -p -e "require('./package.json').version")"
   git push --tags -f HEAD:"$TRAVIS_BRANCH"
   npm publish --tag "$TRAVIS_BRANCH"
