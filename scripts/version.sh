c684feb418a256732026c0e8cad80a885fa813da
Michael <mi666gan@gmail.com>
2020-07-08 10:33:04 +0300

    implement flex justify (#158)

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-if ! yarn version check
+#!/bin/bash
+if ! [[ $(git log -1 --pretty=%B) =~ ^Release\ .+$ ]]
 then
   mkdir -p .yarn/versions
   cp yarn-versions/"$TRAVIS_BRANCH".yml .yarn/versions/
   yarn version apply --all
-  VERSION=$(node -p -e "require('./packages/core/package.json').version")
   git commit -am "Release $VERSION"
-  yarn workspaces foreach npm publish --tag "$TRAVIS_BRANCH"
+  VERSION=$(node -p -e "require('./packages/core/package.json').version")
   git push -f origin HEAD:"$TRAVIS_BRANCH"
+  yarn workspaces foreach npm publish --tag "$TRAVIS_BRANCH"
 fi
