07de5d097688babb5874b15d0520c5d47931afa1
Mikhail Boyarov <mi6gan@mail.ru>
2020-07-08 04:08:33 +0300

    migrate to yarn 2

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,6 +3,5 @@ mkdir -p .yarn/versions
 cp yarn-versions/"$TRAVIS_BRANCH".yml .yarn/versions/
 yarn version apply --all
 VERSION=$(node -p -e "require('./package.json').version")
-git commit -am "Release $VERSION"
-git tag "$VERSION"
-git push --tags -f origin HEAD:"$TRAVIS_BRANCH"
+git tag -am "Release $VERSION" "$VERSION"
+git push --follow-tags origin HEAD:"$TRAVIS_BRANCH"
