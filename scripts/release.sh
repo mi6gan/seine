e83c7e385684f4ab61ac7b6dea9d63fa257d1dbd
Mikhail Boyarov <mi6gan@mail.ru>
2020-09-12 21:41:20 +0300

    Release 0.3.296

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,24 +2,24 @@
 mkdir -p .yarn/versions
 echo -e \
 releases:'\n'\
- @seine/charts: patch'\n'\
- @seine/charts-editor: patch'\n'\
- @seine/content: patch'\n'\
- @seine/core: patch'\n'\
- @seine/draft: patch'\n'\
- @seine/draft-editor: patch'\n'\
- @seine/editor: patch'\n'\
- @seine/styles: patch'\n'\
- @seine/tables: patch'\n'\
- @seine/tables-editor: patch'\n'\
- @seine/testbox: patch'\n'\
- @seine/ui: patch':w\n'\
- react-color.macro: patch'\n'\
+'  '@seine/charts: patch'\n'\
+'  '@seine/charts-editor: patch'\n'\
+'  '@seine/content: patch'\n'\
+'  '@seine/core: patch'\n'\
+'  '@seine/draft: patch'\n'\
+'  '@seine/draft-editor: patch'\n'\
+'  '@seine/editor: patch'\n'\
+'  '@seine/styles: patch'\n'\
+'  '@seine/tables: patch'\n'\
+'  '@seine/tables-editor: patch'\n'\
+'  '@seine/testbox: patch'\n'\
+'  '@seine/ui: patch':w\n'\
+'  'react-color.macro: patch'\n'\
 undecided:'\n'\
- - seine\
+'  '- seine\
 > .yarn/versions/release.yml
 yarn version apply --all
-yarn workspaces foreach npm publish
 VERSION=$(node -p -e "require('./packages/core/package.json').version")
 git commit -am "Release $VERSION"
-git push -f origin HEAD:latest
+yarn workspaces foreach npm publish
+# git push -f origin HEAD:latest
