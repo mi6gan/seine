f42e54e7bd9d12e414bde458bd9ceb2fae3bc1d5
Mikhail Boyarov <mi6gan@mail.ru>
2020-11-16 14:47:41 +0300

    #312 Allow to set units separately for each of values in constraint sizing

--- packages/editor/src/ui/SidebarSelect.js
+++ packages/editor/src/ui/SidebarSelect.js
@@ -7,7 +7,7 @@ const SidebarSelect = styled(MuiSelect)`
   .MuiSelect-select {
     font-size: ${({ theme }) => theme.typography.caption.fontSize};
     padding-top: 0;
-    padding-bottom: 0;
+    padding-bottom: 5px;
   }
 `;
 
