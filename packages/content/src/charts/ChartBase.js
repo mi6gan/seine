0bdf529d2e96c1248954f0921c58c876479ae898
Mikhail Boyarov <mi6gan@mail.ru>
2020-11-26 21:54:57 +0300

    Fix chart selection in editor

--- packages/content/src/charts/ChartBase.js
+++ packages/content/src/charts/ChartBase.js
@@ -2,11 +2,32 @@
 import * as React from 'react';
 import { Chart } from '@devexpress/dx-react-chart-material-ui';
 
+import {
+  defaultBarChartFormat,
+  defaultChartFormat,
+  defaultColumnChartFormat,
+  defaultItemFormat,
+  defaultLineChartFormat,
+  defaultPieChartFormat,
+} from '@seine/core';
+
+const formatKeys = Object.keys({
+  ...defaultItemFormat,
+  ...defaultChartFormat,
+  ...defaultBarChartFormat,
+  ...defaultColumnChartFormat,
+  ...defaultPieChartFormat,
+  ...defaultLineChartFormat,
+});
+
 // eslint-disable-next-line
-const ChartBase = React.forwardRef(({ className, data, children }, ref) => (
-  <Chart className={className} data={data} ref={ref}>
-    {children}
-  </Chart>
+const ChartBase = React.forwardRef((props, ref) => (
+  <Chart
+    ref={ref}
+    {...Object.fromEntries(
+      Object.entries(props).filter(([k]) => !formatKeys.includes(k))
+    )}
+  />
 ));
 
 export default ChartBase;
