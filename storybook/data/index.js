b3a2a02984d5c061e8616879b1ec7375146ded5b
Michael <mi666gan@gmail.com>
2020-12-09 18:46:49 -0500

    bream-348 (#340)
    * Release 0.4.71
    
    * bream#348 Пустое пространство после таблиц
    
    Co-authored-by: mi6gan <>
    Co-authored-by: Michael <mi6gan@mail.ru>

--- storybook/data/index.js
+++ storybook/data/index.js
@@ -4,3 +4,4 @@ export { default as page_5eec4cb7 } from './page-5eec4cb7.json';
 export { default as page_ae102d9a } from './page-ae102d9a.json';
 export { default as page_286a2b56 } from './page-286a2b56.json';
 export { default as page_044d4dca } from './page-044d4dca.json';
+export { default as page_7352724a } from './page-7352724a.json';
