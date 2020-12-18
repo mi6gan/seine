af2e961640a386cdca2ca5d4aa6ee636e47c13ed
Michael <mi6gan@mail.ru>
2020-12-19 00:27:12 +0300

    Add automation tests

--- storybook/Storyshots.test.js
+++ storybook/Storyshots.test.js
@@ -1,10 +1,9 @@
-import initStoryshots, { renderWithOptions } from '@storybook/addon-storyshots';
+// @flow
+import initStoryShots from '@storybook/addon-storyshots';
 import { mount } from 'enzyme';
 
 jest.mock('draft-js/lib/generateRandomKey', () => () => 'mock');
 
-initStoryshots({
-  test: renderWithOptions({
-    renderer: mount,
-  }),
+initStoryShots({
+  renderer: mount,
 });
--- storybook/setupTests.js
+++ storybook/setupTests.js
@@ -1,3 +1,4 @@
+// @flow
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ResizeObserver from 'resize-observer-polyfill';
