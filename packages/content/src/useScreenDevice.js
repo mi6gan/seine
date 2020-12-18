162437be5a1a31f0555cc3a0ecd94b35fe390be5
Michael <mi6gan@mail.ru>
2020-12-18 15:53:45 +0300

    bream#1261 Вылетают тесты на iPhone (генератор)

--- packages/content/src/useScreenDevice.js
+++ packages/content/src/useScreenDevice.js
@@ -28,10 +28,10 @@ function useMatchMedia(alias, setDevice, start = null, end = null) {
         }
       };
 
-      mql.addEventListener('change', handler);
+      mql.addListener('change', handler);
 
       return () => {
-        mql.removeEventListener('change', handler);
+        mql.removeListener('change', handler);
       };
     }
   });
