614c5d21f0614a0680602f6778626e2e595778fb
Michael <mi6gan@mail.ru>
2020-12-18 09:32:21 +0300

    Add editor component docs

--- storybook/Description.js
+++ storybook/Description.js
@@ -1,9 +1,21 @@
 // @flow
+import * as React from 'react';
 import doctrine from 'doctrine';
+import {
+  Description as DefaultDescription,
+  DocsContext,
+} from '@storybook/addon-docs/blocks';
 
 // eslint-disable-next-line
 export default function Description({ of }) {
-  const { tags } = doctrine.parse(of.__docgenInfo.description);
+  const { parameters } = React.useContext(DocsContext);
+  const { tags } = doctrine.parse(
+    (of === '.' ? parameters.component : of).__docgenInfo.description
+  );
   const description = tags.find(({ title }) => title === 'description') || null;
-  return description && description.description;
+  return (
+    <DefaultDescription>
+      {description && description.description}
+    </DefaultDescription>
+  );
 }
