a2b6b76a8fe2cd975fae040466555d0fcf3a7b86
Michael <mi666gan@gmail.com>
2021-06-11 09:36:42 +0300

    Fix shape svg sizing on export (#385)

--- packages/content/src/shapes/RootShape.js
+++ packages/content/src/shapes/RootShape.js
@@ -6,7 +6,14 @@ import useElementOnlyProps from '../useElementOnlyProps';
 import { defaultItemFormat } from '@seine/core';
 
 const RootShape = React.forwardRef(function RootShape(props, ref) {
-  return <svg {...useElementOnlyProps(props, defaultItemFormat)} ref={ref} />;
+  return (
+    <svg
+      {...useElementOnlyProps(props, defaultItemFormat)}
+      ref={ref}
+      width={'100%'}
+      height={'100%'}
+    />
+  );
 });
 
 export default RootShape;
